de77dd6bf1e37a2855031a6efec49f010b037919
Anastasia Ivanova <amastasia.ivanova@minsk.leveris.com>
2020-12-14 22:56:24 +0300

    fix: linter warning

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -43,7 +43,7 @@ function getComposition(/* f, g */) {
  *
  */
 function getPowerFunction(exponent) {
-  return function (number) {
+  return function innerGetPowerFunction(number) {
     return number ** exponent;
   };
 }
